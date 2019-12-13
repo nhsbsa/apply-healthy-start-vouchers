6051d1211fdc065b58382dec3fd0b22c5b0d7aed
Stephen Hill <stephen.hill9@nhs.net>
2019-12-13 14:47:09 +0000

    Removed request from dependencies

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const moment = require('moment');
-const request = require('request');
-
-var addressLookupKey = 'fVVL6DvtzkGhkVo7Whyxsg22465';
 
 // Add your routes here - above the module.exports line
 
