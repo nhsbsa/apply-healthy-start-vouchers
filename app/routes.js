ab94cbca831abfd935f5cb24c6dd1babdd8fba40
Stephen Hill <stephen.hill9@nhs.net>
2020-05-07 14:18:44 +0100

    Removed v5 and altered flow

--- app/routes.js
+++ app/routes.js
@@ -38,23 +38,6 @@ router.post('/v3/declaration', function (req, res) {
 
 })
 
-// V4 Declaration
-
-router.post('/v4/declaration', function (req, res) {
-
-  var emailAddress = req.session.data['emailaddress'];
-
-  var refNo = 'HDJ2123F';
-  var firstName = req.session.data['firstname'];
-  var paymentAmount = '£12.40';
-  var childrenUnder4Payment = '£3.10 every week for each of your children between the ages of 1 and 4 years old.';
-
-  notify.sendEmail('f2653d20-a1d6-46be-978e-a6234cb6b674', emailAddress, { personalisation: { 'refNo': refNo, 'firstName': firstName, 'paymentAmount': paymentAmount, 'childrenUnder4Payment': childrenUnder4Payment } } );
-
-  res.redirect('/v4/apply/confirmation-successful');
-
-})
-
 // Add your routes here - above the module.exports line
 
 module.exports = router;
@@ -974,6 +957,27 @@ router.post('/v3/feedback', function (req, res) {
 // APPLY (VERSION 4)
 // ********************************
 
+// What is your national insurance number?
+
+router.post('/v4/national-insurance-number', function (req, res) {
+
+  var nationalinsurancenumber = req.session.data['nationalinsurancenumber'].replace(/\s+/g, '');
+
+  if (nationalinsurancenumber) {
+
+    if (nationalinsurancenumber === 'QQ123456C') {
+      res.redirect('/v4/apply/name')
+    } else {
+      res.redirect('/v4/apply/kickouts/not-eligible-national-insurance-number')
+    }
+
+  }
+  else {
+    res.redirect('/v4/apply/kickouts/national-insurance-number')
+  }
+
+})
+
 // What is your name?
 
 router.post('/v4/name', function (req, res) {
@@ -1007,31 +1011,10 @@ router.post('/v4/date-of-birth', function (req, res) {
 
 
   if (dateofbirthday && dateofbirthmonth && dateofbirthyear) {
-    res.redirect('/v4/apply/national-insurance-number')
+    res.redirect('/v4/apply/address')
   }
   else {
     res.redirect('/v4/apply/date-of-birth')
-  }
-
-})
-
-// What is your national insurance number?
-
-router.post('/v4/national-insurance-number', function (req, res) {
-
-  var nationalinsurancenumber = req.session.data['nationalinsurancenumber'].replace(/\s+/g, '');
-
-  if (nationalinsurancenumber) {
-
-    if (nationalinsurancenumber === 'QQ123456C') {
-      res.redirect('/v4/apply/address')
-    } else {
-      res.redirect('/v4/apply/kickouts/not-eligible-national-insurance-number')
-    }
-
-  }
-  else {
-    res.redirect('/v4/apply/kickouts/national-insurance-number')
   }
 
 })
@@ -1048,20 +1031,10 @@ router.post('/v4/address', function (req, res) {
   var postcode = req.session.data['postcode']
 
   if (addressline1 && towncity && postcode) {
-    res.redirect('/v4/apply/email-address')
+    res.redirect('/v4/apply/bank-details')
   } else {
     res.redirect('/v4/apply/address')
   }
-
-})
-
-// What is your email address?
-
-router.post('/v4/email-address', function (req, res) {
-
-  var emailaddress = req.session.data['emailaddress']
-
-  res.redirect('/v4/apply/bank-details')
 
 })
 
@@ -1088,122 +1061,14 @@ router.post('/v4/check-your-answers', function (req, res) {
   res.redirect('/v4/apply/declaration')
 })
 
+// Declaration
+
+router.post('/v4/declaration', function (req, res) {
+  res.redirect('/v4/apply/confirmation-successful')
+})
+
 // Feedback
 
 router.post('/v4/feedback', function (req, res) {
   res.redirect('/v4/feedback')
-})
-
-// ********************************
-// APPLY (VERSION 5)
-// ********************************
-
-// What is your name?
-
-router.post('/v5/name', function (req, res) {
-
-  var firstname = req.session.data['firstname']
-  var lastname = req.session.data['lastname']
-
-  if (firstname && lastname) {
-    res.redirect('/v5/apply/date-of-birth')
-  }
-  else {
-    res.redirect('/v5/apply/name')
-  }
-
-})
-
-// Date of birth
-
-router.post('/v5/date-of-birth', function (req, res) {
-
-  var dateofbirthday = req.session.data['dateofbirthday']
-  var dateofbirthmonth = req.session.data['dateofbirthmonth']
-  var dateofbirthyear = req.session.data['dateofbirthyear']
-
-  var dob = new Date(dateofbirthyear, dateofbirthmonth, dateofbirthday);
-  var ageDate =  new Date(today - dob.getTime())
-  var temp = ageDate.getFullYear();
-  var yrs = Math.abs(temp - 1970);
-
-  req.session.data.yrs = yrs;
-
-
-  if (dateofbirthday && dateofbirthmonth && dateofbirthyear) {
-    res.redirect('/v5/apply/national-insurance-number')
-  }
-  else {
-    res.redirect('/v5/apply/date-of-birth')
-  }
-
-})
-
-// What is your national insurance number?
-
-router.post('/v5/national-insurance-number', function (req, res) {
-
-  var nationalinsurancenumber = req.session.data['nationalinsurancenumber'].replace(/\s+/g, '');
-
-  if (nationalinsurancenumber) {
-    res.redirect('/v5/apply/address')
-  }
-  else {
-    res.redirect('/v5/apply/kickouts/national-insurance-number')
-  }
-
-})
-
-// What is your address?
-
-router.post('/v5/address', function (req, res) {
-
-  delete req.session.data['selectaddress']
-
-  var addressline1 = req.session.data['addressline1']
-  var addressline2 = req.session.data['addressline2']
-  var towncity = req.session.data['towncity']
-  var postcode = req.session.data['postcode']
-
-  if (addressline1 && towncity && postcode) {
-    res.redirect('/v5/apply/bank-details')
-  } else {
-    res.redirect('/v5/apply/address')
-  }
-
-})
-
-// Bank Details
-
-router.post('/v5/bank-details', function (req, res) {
-
-  var accountName = req.session.data['accountname']
-  var sortCode = req.session.data['sortcode']
-  var accountNumber = req.session.data['accountnumber']
-
-  if (accountName && sortCode && accountNumber){
-    res.redirect('/v5/apply/check-your-answers')    
-  }
-  else {
-    res.redirect('/v5/apply/bank-details')
-  }
-
-})
-
-// Check your answers
-
-router.post('/v5/check-your-answers', function (req, res) {
-  res.redirect('/v5/apply/declaration')
-})
-
-// Check your answers
-
-router.post('/v5/declaration', function (req, res) {
-  res.redirect('/v5/apply/confirmation-pending')
-})
-
-// Feedback
-
-router.post('/v5/feedback', function (req, res) {
-  res.redirect('/v5/feedback')
 })
