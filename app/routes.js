60590f2a9353e81670562a8b5bced580db1c281c
Stephen Hill <stephen.hill9@nhs.net>
2020-08-13 14:40:20 +0100

    Updated regex to allow for no spaces

--- app/routes.js
+++ app/routes.js
@@ -1456,7 +1456,7 @@ router.get('/v5/find-address', function (req, res) {
   var houseNumberName = req.session.data['housenumber']
   var postcode = req.session.data['postcode']
 
-  const regex = RegExp('^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$');
+  const regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
 
   if (regex.test(postcode) === true) {
 
