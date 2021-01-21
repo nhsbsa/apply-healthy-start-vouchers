12476163d9e705e06d85be18913462b5b94ccf58
StephenHill-NHSBSA <stephen.hill9@nhs.net>
2021-01-21 12:30:30 +0000

    Added v10 (validate against last name, NINO, dob, 1st line of address & postcode)

--- app/routes.js
+++ app/routes.js
@@ -19,33 +19,55 @@ const axios = require('axios');
 // PERSONAS
 // ****************************************
 
-// Charlie
+// Charlie Smith
 
 // National Insurance number = AB 12 34 56 A
+// Date of birth = 01 / 01 / 2000
+// 1st Line of Address = 55 Peachfield Road
+// Postcode = LL67 3SN
 // Pregnant? = YES
 // Children Under 1? = NO
 // Children Between 1 & 4? = YES
 
-// Riley
+// Riley Jones
 
 // National Insurance number = CD 65 43 21 B
+// Date of birth = 02 / 02 / 1999
+// 1st Line of Address = 49 Park Terrace
+// Postcode = NR33 4GT
 // Pregnant? = NO
 // Children Under 1? = YES
 // Children Between 1 & 4? = NO
 
-// Alex
+// Alex Johnson
 
 // National Insurance number = EF 21 43 65 C
+// Date of birth = 03 / 03 / 1998
+// 1st Line of Address = 140 Cambridge Road
+// Postcode = AB55 8NL
 // Pregnant? = YES
 // Children Under 1? = NO
 // Children Between 1 & 4? = NO
 
-// Tony
+// Tony Brown
 
 // National Insurance number = GH 56 34 12 D
+// Date of birth = 04 / 04 / 1997
+// 1st Line of Address = 124 West Lane
+// Postcode = KA24 8PE
 // Pregnant? = NO
 // Children Under 1? = NO
 // Children Between 1 & 4? = YES
+
+// Samantha Miller
+
+// National Insurance number = IJ 87 65 43 D
+// Date of birth = 05 / 05 / 1996
+// 1st Line of Address = 85 Broad Street
+// Postcode = WA4 3AS
+// Pregnant? = NO
+// Children Under 1? = NO
+// Children Between 1 & 4? = NO
 
 // ****************************************
 // NOTIFICATIONS
@@ -505,7 +527,7 @@ router.post('/v9/check-your-answers', function (req, res) {
     if (pregnant === "yes") {
 
     // UNCOMMENT OUT WHEN WE DO VITAMINS
-    
+
     //  notifyClient.sendEmail('a555749d-0f67-4fbd-b787-0bb158eb34bc', emailAddress, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': pregnancyPayment, 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment, 'vitamin_start_date': vitStart, 'vitamin_end_date': vitEnd, 'vitaminTypeWomen': vitTypeWomen, 'vitaminTypeChildren': "" }, reference: null })
     //  .then(response => { console.log(response); res.redirect('/v9/apply/confirmation-successful'); })
     //  .catch(err => console.error(err))
@@ -552,7 +574,89 @@ router.post('/v9/check-your-answers', function (req, res) {
 
 })
 
+// V10 Check Your Answers
 
+// N.B ALL PERSONALISATION VARIABLES NEED TO BE THERE, IF THEY'RE NOT REQUIRED YOU STILL NEED TO SEND AN EMPTY STRING ""
+
+router.post('/v10/check-your-answers', function (req, res) {
+
+  var contact = req.session.data['contact'];
+  var emailAddress = req.session.data['emailaddress'];
+  var mobilePhoneNumber = req.session.data['mobilephonenumber'];
+  var pregnant = req.session.data['pregnant']
+  var firstName = req.session.data['firstname'];
+  
+  if (pregnant === "yes") {
+
+  var refNo = 'HDJ2123F';
+  var paymentAmount = '£24.80';
+  var pregnancyPayment = '\n* £12.40 for a pregnancy';
+  var childrenUnder4Payment = '\n* £12.40 for children between 1 and 4';
+
+  var vitStart = moment().format('D MMMM YYYY');
+  var vitEnd = moment().add(8, 'weeks').format("D MMMM YYYY");
+  var vitTypeWomen = '\n* 1 pack(s) of vitamins for women';
+
+  } else {
+
+  var refNo = 'HDJ2123F';
+  var paymentAmount = '£12.40';
+  var childrenUnder4Payment = '\n* £12.40 for children between 1 and 4';
+  
+  }
+
+  if (emailAddress) {
+
+
+    if (pregnant === "yes") {
+
+    // UNCOMMENT OUT WHEN WE DO VITAMINS
+    
+    //  notifyClient.sendEmail('a555749d-0f67-4fbd-b787-0bb158eb34bc', emailAddress, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': pregnancyPayment, 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment, 'vitamin_start_date': vitStart, 'vitamin_end_date': vitEnd, 'vitaminTypeWomen': vitTypeWomen, 'vitaminTypeChildren': "" }, reference: null })
+    //  .then(response => { console.log(response); res.redirect('/v10/apply/confirmation-successful'); })
+    //  .catch(err => console.error(err))
+
+    notifyClient.sendEmail('152bd9a2-a79f-4e4f-8bfe-84654ffed6fb', emailAddress, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': "", 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment }, reference: null })
+    .then(response => { console.log(response); res.redirect('/v10/apply/confirmation-successful'); })
+    .catch(err => console.error(err))
+
+    } else {
+
+      notifyClient.sendEmail('152bd9a2-a79f-4e4f-8bfe-84654ffed6fb', emailAddress, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': "", 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment }, reference: null })
+      .then(response => { console.log(response); res.redirect('/v10/apply/confirmation-successful'); })
+      .catch(err => console.error(err))
+  
+    }
+
+  }
+  else if (mobilePhoneNumber) {
+
+    if (pregnant === "yes") {
+
+      // notifyClient.sendSms('fa19ba1e-138c-456c-9c11-791f772a4975', mobilePhoneNumber, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': pregnancyPayment, 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment, 'vitamin_start_date': vitStart, 'vitamin_end_date': vitEnd, 'vitaminTypeWomen': vitTypeWomen, 'vitaminTypeChildren': "" }, reference: null })
+      // .then(response => { console.log(response); res.redirect('/v10/apply/confirmation-successful'); })
+      // .catch(err => console.error(err))
+
+      res.redirect('/v10/apply/confirmation-successful');
+
+    } else {
+
+      // notifyClient.sendSms('e9299ebf-725c-4d8a-86c6-b28c0ef0028a', mobilePhoneNumber, { personalisation: { 'reference_number': refNo, 'first_name': firstName, 'payment_amount': paymentAmount, 'pregnancy_payment': "", 'children_under_1_payment': "", 'children_under_4_payment': childrenUnder4Payment }, reference: null })
+      // .then(response => { console.log(response); res.redirect('/v10/apply/confirmation-successful'); })
+      // .catch(err => console.error(err))
+
+      res.redirect('/v10/apply/confirmation-successful');
+  
+    }
+
+  } else if (!emailAddress && !mobilePhoneNumber) {
+    res.redirect('/v10/apply/confirmation-successful');
+
+  } else {
+    res.redirect('/v10/apply/check-your-answers')
+  }
+
+})
 
 // Add your routes here - above the module.exports line
 
@@ -3148,86 +3252,6 @@ router.post('/cookies/cookie-confirmation', function (req, res) {
 })
 
 // ********************************
-// APPLY (FUTURE)
-// ********************************
-
-// Do you get Healthy Start vouchers at the moment?
-router.post('/future/v1/who-applying-for', function (req, res) {
-
-  var applyingfor = req.session.data['applyingfor']
-
-  if (applyingfor === "myself") {
-    res.redirect('/future/v1/apply/do-you-have-NHS-login')
-  }
-  else if (applyingfor === "someoneelse") {
-    res.redirect('/v6/apply/national-insurance-number')
-  }
-  else {
-    res.redirect('/future/v1/apply/who-applying-for')
-  }
-
-})
-
-// Do you have an NHS login?
-router.post('/future/v1/do-you-have-NHS-login', function (req, res) {
-
-  var nhslogin = req.session.data['nhslogin']
-
-  if (nhslogin === "yes") {
-    res.redirect('/future/v1/apply/email')
-  }
-  else if (nhslogin === "no") {
-    res.redirect('/v6/apply/national-insurance-number')
-  }
-  else {
-    res.redirect('/future/v1/apply/who-applying-for')
-  }
-
-})
-
-// Email
-router.post('/future/v1/email', function (req, res) {
-
-  var email = req.session.data['email']
-
-  if (email) {
-    res.redirect('/future/v1/apply/password')
-  }
-  else {
-    res.redirect('/future/v1/apply/email')
-  }
-
-})
-
-// Password
-router.post('/future/v1/password', function (req, res) {
-
-  var password = req.session.data['password']
-
-  if (password) {
-    res.redirect('/future/v1/apply/check-code')
-  }
-  else {
-    res.redirect('/future/v1/apply/password')
-  }
-
-})
-
-// Check code
-router.post('/future/v1/security-code', function (req, res) {
-
-  var securitycode = req.session.data['securitycode']
-
-  if (securitycode) {
-    res.redirect('/future/v1/apply/authorise')
-  }
-  else {
-    res.redirect('/future/v1/apply/check-code')
-  }
-
-})
-
-// ********************************
 // APPLY (VERSION 9)
 // ********************************
 
@@ -3529,4 +3553,401 @@ router.post('/v9/feedback', function (req, res) {
   res.redirect('/v9/feedback')
 })
 
+// ********************************
+// APPLY (VERSION 10)
+// ********************************
+
+// What is your name?
+
+router.post('/v10/name', function (req, res) {
+
+  var firstname = req.session.data['firstname']
+  var lastname = req.session.data['lastname']
+
+  if (firstname && lastname) {
+    res.redirect('/v10/apply/address')
+  }
+  else {
+    res.redirect('/v10/apply/name')
+  }
+
+})
+
+// Find your address
+
+router.get('/v10/find-address', function (req, res) {
+
+  var houseNumberName = req.session.data['housenumber']
+  var postcode = req.session.data['postcode']
+
+  const regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
+
+  if (regex.test(postcode) === true) {
+
+
+
+    if (houseNumberName) {
+
+      axios.get("https://api.getAddress.io/find/" + postcode + "/" + houseNumberName + "?api-key="+ process.env.POSTCODEAPIKEY)
+      .then(response => {
+        console.log(response.data.addresses);
+        var items = response.data.addresses;
+        res.render('v10/apply/select-address', {items: items});
+      })
+      .catch(error => {
+        console.log(error);
+        res.redirect('/v10/apply/no-address-found')
+      });
+
+    } else {
+
+      axios.get("https://api.getAddress.io/find/" + postcode + "?api-key="+ process.env.POSTCODEAPIKEY)
+      .then(response => {
+        console.log(response.data.addresses);
+        var items = response.data.addresses;
+        res.render('v10/apply/select-address', {items: items});
+      })
+      .catch(error => {
+        console.log(error);
+        res.redirect('/v10/apply/no-address-found')
+      });
+
+    }
+    
+    
+
+
+
+
+  
+
+  } else {
+    res.redirect('/v10/apply/find-address')
+  }
+
+})
+
+// Select your address
+
+router.get('/v10/select-address', function (req, res) {
+
+  var selectaddress = req.session.data['selectaddress']
+
+  if (selectaddress === 'none') {
+
+    delete req.session.data['addressline1']
+    delete req.session.data['addressline2']
+    delete req.session.data['towncity']
+    delete req.session.data['postcode']
+
+    res.redirect('/v10/apply/address')
+  } else if (selectaddress) {
+    res.redirect('/v10/apply/national-insurance-number')
+  } else {
+    res.redirect('/v10/apply/select-address')
+  }
+
+})
+
+// What is your address?
+
+router.post('/v10/address', function (req, res) {
+
+  delete req.session.data['selectaddress']
+
+  var addressline1 = req.session.data['addressline1']
+  var addressline2 = req.session.data['addressline2']
+  var towncity = req.session.data['towncity']
+  var postcode = req.session.data['postcode'].replace(/\s+/g, '').toUpperCase()
+
+  if (addressline1 && towncity && postcode) {
+    res.redirect('/v10/apply/national-insurance-number')
+  } else {
+    res.redirect('/v10/apply/address')
+  }
+
+})
+
+// What is your national insurance number?
+
+router.post('/v10/national-insurance-number', function (req, res) {
+
+  var nationalinsurancenumber = req.session.data['nationalinsurancenumber'].toUpperCase().replace(/\s+/g, '');
+
+  if (nationalinsurancenumber) {
+    res.redirect('/v10/apply/date-of-birth')
+  }
+  else {
+    res.redirect('/v10/apply/kickouts/national-insurance-number')
+  }
+
+})
+
+// Date of birth
+
+router.post('/v10/date-of-birth', function (req, res) {
+
+  var dateofbirthday = req.session.data['dateofbirthday']
+  var dateofbirthmonth = req.session.data['dateofbirthmonth']
+  var dateofbirthyear = req.session.data['dateofbirthyear']
+
+  var dob = moment((dateofbirthday + '-' + dateofbirthmonth + '-' + dateofbirthyear), "DD-MM-YYYY");
+  var dateofbirth = dob.format();
+
+  var lastname = req.session.data['lastname']
+  var addressline1 = req.session.data['addressline1']
+  var postcode = req.session.data['postcode'].replace(/\s+/g, '').toUpperCase()
+  var nationalinsurancenumber = req.session.data['nationalinsurancenumber'].toUpperCase().replace(/\s+/g, '');
+
+  console.log(lastname);
+  console.log(addressline1);
+  console.log(postcode);
+  console.log(nationalinsurancenumber);
+  console.log(dateofbirth);
+
+
+  if (dateofbirthday && dateofbirthmonth && dateofbirthyear) {
+
+    if (lastname === 'Smith' && nationalinsurancenumber === 'AB123456A' && dateofbirth === '2000-01-01T00:00:00+00:00' && addressline1 === '55 Peachfield Road' && postcode === 'LL673SN') {
+        res.redirect('/v10/apply/nationality')
+    } else if (lastname === 'Jones' && nationalinsurancenumber === 'CD654321B' && dateofbirth === '1999-02-02T00:00:00+00:00' && addressline1 === '49 Park Terrace' && postcode === 'NR334GT') {
+        res.redirect('/v10/apply/nationality')
+    } else if (lastname === 'Johnson' && nationalinsurancenumber === 'EF214365C' && dateofbirth === '1998-03-03T00:00:00+00:00' && addressline1 === '140 Cambridge Road' && postcode === 'AB558NL') {
+        res.redirect('/v10/apply/nationality')
+    } else if (lastname === 'Brown' && nationalinsurancenumber === 'GH563412D' && dateofbirth === '1997-04-04T00:00:00+00:00' && addressline1 === '124 West Lane' && postcode === 'KA248PE') {
+        res.redirect('/v10/apply/nationality')
+    } else if (lastname === 'Miller' && nationalinsurancenumber === 'IJ876543D' && dateofbirth === '1996-05-05T00:00:00+00:00' && addressline1 === '85 Broad Street' && postcode === 'WA43AS') {
+        res.redirect('/v10/apply/kickouts/confirmation-no-match')
+    } else {
+        res.redirect('/v10/apply/kickouts/confirmation-no-match')
+    }   
+
+  }
+  else {
+    res.redirect('/v10/apply/date-of-birth')
+  }
+
+})
+
+// What is your nationality?
+
+router.post('/v10/nationality', function (req, res) {
+
+  var nationality = req.session.data['input-autocomplete']
+
+  if (nationality) {
+    res.redirect('/v10/apply/are-you-pregnant')
+  }
+  else {
+    res.redirect('/v10/apply/nationality')
+  }
+
+})
+
+// Are you pregnant?
+
+router.post('/v10/are-you-pregnant', function (req, res) {
+
+  var pregnant = req.session.data['pregnant']
+
+  if (pregnant === "yes") {
+    res.redirect('/v10/apply/due-date')
+  }
+  else if (pregnant === "no") {
+    req.session.data.lessThanTenWeeksPregnant = true;
+    res.redirect('/v10/apply/find-address')
+  }
+  else {
+    res.redirect('/v10/apply/are-you-pregnant')
+  }
+
+})
+
+// Are you pregnant? > Due Date
+
+router.post('/v10/due-date', function (req, res) {
+
+  var duedateday = req.session.data['duedateday']
+  var duedatemonth = req.session.data['duedatemonth']
+  var duedateyear = req.session.data['duedateyear']
+
+  var duedate = moment(duedateyear + '-' + duedatemonth + '-' + duedateday);
+
+  var today = moment();
+
+  var fulltermpregnancy = moment().add(42, 'weeks'); // 42 weeks from today is a full term pregnancy
+  var tenweekspregnant = moment().add(32, 'weeks'); // 42 weeks from today is a full term pregnancy - 10 weeks = 32 weeks
+
+  
+  if (duedateday && duedatemonth && duedateyear) {
+
+    if (duedate < today) {
+      res.redirect('/v10/apply/due-date')
+    } else if (duedate > fulltermpregnancy) {
+      res.redirect('/v10/apply/due-date')
+    } else {
+
+      if (duedate >= tenweekspregnant && duedate <= fulltermpregnancy) {
+        req.session.data.lessThanTenWeeksPregnant = true;
+      } else {
+        req.session.data.lessThanTenWeeksPregnant = false;
+      }
+
+      res.redirect('/v10/apply/find-address')
+    }
+
+  }
+  else {
+    res.redirect('/v10/apply/due-date')
+  }
+
+})
+
+
+// What is your email address?
+
+router.post('/v10/email-address', function (req, res) {
+
+  var emailaddress = req.session.data['emailaddress']
+
+  res.redirect('/v10/apply/mobile-phone-number')
+
+})
+
+// What is your mobile phone number?
+
+router.post('/v10/mobile-phone-number', function (req, res) {
+
+  var mobilePhoneNumber = req.session.data['mobilephonenumber']
+
+  res.redirect('/v10/apply/check-your-answers')
+
+})
+
+// Contact Preferences
+
+router.post('/v10/contact-preferences', function (req, res) {
+
+  var contact = req.session.data['contact']
+  var emailAddress = req.session.data['emailaddress']
+  var mobile = req.session.data['mobile']
+
+  if (contact) {
+
+    if (emailAddress || mobile || contact === 'NONE'){
+      res.redirect('/v10/apply/bank-details')    
+    }
+    else {
+      res.redirect('/v10/apply/contact-preferences')
+    }
+
+  }
+  else {
+    res.redirect('/v10/apply/contact-preferences')
+  }
+
+})
+
+// Bank Details
+
+router.post('/v10/bank-details', function (req, res) {
+
+  var accountName = req.session.data['accountname']
+  var sortCode = req.session.data['sortcode']
+  var accountNumber = req.session.data['accountnumber']
+
+  if (accountName && sortCode && accountNumber){
+    res.redirect('/v10/apply/check-your-answers')    
+  }
+  else {
+    res.redirect('/v10/apply/bank-details')
+  }
+
+})
+
+// Feedback
+
+router.post('/v10/feedback', function (req, res) {
+  res.redirect('/v10/feedback')
+})
+
+// ********************************
+// APPLY (FUTURE)
+// ********************************
+
+// Do you get Healthy Start vouchers at the moment?
+router.post('/future/v1/who-applying-for', function (req, res) {
+
+  var applyingfor = req.session.data['applyingfor']
+
+  if (applyingfor === "myself") {
+    res.redirect('/future/v1/apply/do-you-have-NHS-login')
+  }
+  else if (applyingfor === "someoneelse") {
+    res.redirect('/v6/apply/national-insurance-number')
+  }
+  else {
+    res.redirect('/future/v1/apply/who-applying-for')
+  }
+
+})
+
+// Do you have an NHS login?
+router.post('/future/v1/do-you-have-NHS-login', function (req, res) {
+
+  var nhslogin = req.session.data['nhslogin']
+
+  if (nhslogin === "yes") {
+    res.redirect('/future/v1/apply/email')
+  }
+  else if (nhslogin === "no") {
+    res.redirect('/v6/apply/national-insurance-number')
+  }
+  else {
+    res.redirect('/future/v1/apply/who-applying-for')
+  }
+
+})
+
+// Email
+router.post('/future/v1/email', function (req, res) {
+
+  var email = req.session.data['email']
+
+  if (email) {
+    res.redirect('/future/v1/apply/password')
+  }
+  else {
+    res.redirect('/future/v1/apply/email')
+  }
+
+})
+
+// Password
+router.post('/future/v1/password', function (req, res) {
+
+  var password = req.session.data['password']
+
+  if (password) {
+    res.redirect('/future/v1/apply/check-code')
+  }
+  else {
+    res.redirect('/future/v1/apply/password')
+  }
+
+})
+
+// Check code
+router.post('/future/v1/security-code', function (req, res) {
+
+  var securitycode = req.session.data['securitycode']
+
+  if (securitycode) {
+    res.redirect('/future/v1/apply/authorise')
+  }
+  else {
+    res.redirect('/future/v1/apply/check-code')
+  }
+
+})
 
